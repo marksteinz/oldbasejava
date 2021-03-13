c28fba04410849ec7eb5f7c1109d845adc8440b9
kmark <k.markagent@gmail.com>
2021-03-13 14:53:16 +0300

    Introduce package

--- com/urise/webapp/storage/ArrayStorage.java
+++ com/urise/webapp/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
@@ -19,7 +23,7 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid == uuid) {
                 return storage[i];
             }
