716a9842d844e021ee6defc986c835e68aa54e62
kmark <k.markagent@gmail.com>
2021-04-01 21:01:22 +0300

    Lesson04 Reflection and JUnit

---
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,19 @@
+package com.urise.webapp.storage;
+
+import org.junit.Test;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest{
+
+    @Test
+    public void deleteElementIndex() {
+    }
+
+    @Test
+    public void insertElementIndex() {
+    }
+
+    @Test
+    public void findElementIndex() {
+    }
+}
+
---
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,20 @@
+package com.urise.webapp.storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest{
+
+    @Test
+    public void deleteElementIndex() {
+    }
+
+    @Test
+    public void insertElementIndex() {
+    }
+
+    @Test
+    public void findElementIndex() {
+    }
+}
