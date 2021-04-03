0182be074240a09b06027afff2a66960cfe0eda9
kmark <k.markagent@gmail.com>
2021-04-03 16:27:18 +0300

    HW04

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,36 +1,86 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class AbstractArrayStorageTest {
+public abstract class AbstractArrayStorageTest {
+    private Storage storage;
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final Resume resume1 = new Resume(UUID_1);
+    private static final Resume resume2 = new Resume(UUID_2);
+    private static final Resume resume3 = new Resume(UUID_3);
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
+
+    @Before
+    public void setUp() {
+        storage.clear();
+        storage.save(resume1);
+        storage.save(resume2);
+        storage.save(resume3);
+    }
 
     @Test
     public void size() {
+        assertEquals(3, storage.size());
     }
 
     @Test
     public void save() {
+        assertSame(resume1, storage.get(UUID_1));
+        assertSame(resume2, storage.get(UUID_2));
+        assertSame(resume3, storage.get(UUID_3));
     }
 
     @Test
     public void clear() {
+        storage.clear();
+        assertEquals(0, storage.size());
     }
 
     @Test
     public void update() {
+        storage.update(resume1);
+        assertSame(resume1, storage.get(UUID_1));
     }
 
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
+        storage.delete(UUID_1);
+        storage.get(UUID_1);
     }
 
     @Test
     public void getAll() {
+        Resume[] resumesExcepted = new Resume[]{resume1, resume2, resume3};
+        assertArrayEquals(resumesExcepted, storage.getAll());
     }
 
     @Test
     public void get() {
+        assertSame(resume1, storage.get(UUID_1));
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() {
+        storage.get("dummy");
+    }
+
+    @Test(expected = StorageException.class)
+    public void storageOverflow() {
+        storage.clear();
+        for (int i = 0; i < 10_000; i++) {
+            storage.save(new Resume("uuid" + i));
+        }
+        storage.save(new Resume("uuid" + 10_001));
     }
 }
--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -1,19 +1,9 @@
 package com.urise.webapp.storage;
 
-import org.junit.Test;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest{
 
-    @Test
-    public void deleteElementIndex() {
-    }
-
-    @Test
-    public void insertElementIndex() {
-    }
-
-    @Test
-    public void findElementIndex() {
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
     }
 }
-
