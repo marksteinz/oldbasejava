d9355d2447e42000f42b56ae8d2cc40a98636880
kmark <k.markagent@gmail.com>
2021-05-01 13:12:07 +0300

    Lesson07 enum

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,8 +6,10 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<FK> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     protected abstract FK findElementKey(String uuid);
 
@@ -25,19 +27,23 @@ public abstract class AbstractStorage<FK> implements Storage {
 
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         return getElement(getExistKey(uuid));
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         deleteElement(getExistKey(uuid));
     }
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         String uuid = resume.getUuid();
         saveElement(resume, getNotExistKey(uuid));
     }
 
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         String uuid = resume.getUuid();
         updateElement(resume, getExistKey(uuid));
     }
@@ -47,6 +53,7 @@ public abstract class AbstractStorage<FK> implements Storage {
         if (isExist(key)) {
             return key;
         }
+        LOG.warning("Resume " + uuid + " not exist");
         throw new NotExistStorageException(uuid);
     }
 
@@ -55,11 +62,13 @@ public abstract class AbstractStorage<FK> implements Storage {
         if (!isExist(key)) {
             return key;
         }
+        LOG.warning("Resume " + uuid + " already exist");
         throw new ExistStorageException(uuid);
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = doCopyAll();
         Collections.sort(list);
         return list;
