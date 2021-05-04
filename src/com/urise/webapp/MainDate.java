b00ef4d2425c7fed39805d65e2145317e2f2d183
kmark <k.markagent@gmail.com>
2021-05-04 18:50:28 +0300

    Lesson08 DateTime api

---
+++ src/com/urise/webapp/MainDate.java
@@ -0,0 +1,10 @@
+package com.urise.webapp;
+
+import java.util.Date;
+
+public class MainDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
